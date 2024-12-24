70cbcd6511eabebe4b00d414c5cea4e8678ee28a
Sergio Navarro <s.navarroredondo@gmail.com>
2024-12-24 15:27:47 +0100

    ADD security optimizations to mongoDB sessions
    https://app.clickup.com/t/86976zv3a

--- server/middlewares/mongoDbSessions.mjs
+++ server/middlewares/mongoDbSessions.mjs
@@ -5,11 +5,19 @@ export const storeSessionsInMongoDb = session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
-    cookie: { maxAge: 86400000 }, // 24 hours
+    cookie: { 
+        maxAge: 7200000, // 2 hours
+        secure: process.env.NODE_ENV === 'production', // Secure in production
+        httpOnly: true,
+        sameSite: 'strict'
+    }, 
     store: MongoStore.create({  //* SHOULD ONLY BE USED IN PROD - IN DEV WE SHOULD USE IN-MEMORY STORE OR DEV DB
         mongoUrl: process.env.MONGODB_CONNECTIONSTRING, 
         dbName: "main", 
-        collectionName: "sessions" 
+        collectionName: "sessions",
+        ttl: 7200, // 2 hours in seconds
+        autoRemove: 'interval',
+        autoRemoveInterval: 10 // Minutes between cleanup
     })
 });
 

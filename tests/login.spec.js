7d9eb3a9d632503fedf4f8aca876377c05db1466
Sergio Navarro <s.navarroredondo@gmail.com>
2025-01-06 20:57:11 +0100

    ADD playwright tests to login function

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -26,7 +26,20 @@ test(`should login successfully`, async ({ page }) => {
     await expect(page).toHaveURL(`/user/`);
 });
 
-test(`should fail login`, async ({ page }) => {
+test(`should login successfully WITH random caps in the username`, async ({ page }) => {
+
+    await page.goto(`/login`);
+    
+    await page.fill(`input[type="email"]`, `s.NavaRRoredOndo@gmail.com`)
+
+    await page.fill(`input[type="password"]`, `1234` );
+
+    await page.click(`button[type="submit"]`);
+
+    await expect(page).toHaveURL(`/user/`);
+});
+
+test(`should fail login due to wrong password`, async ({ page }) => {
 
     await page.goto(`/login`);
 
@@ -36,9 +49,46 @@ test(`should fail login`, async ({ page }) => {
 
     await page.click(`button[type="submit"]`);
 
-    await expect(page).toHaveURL(`/failed-login`);
+    const errorMessage = page.locator(`.errorMessage`)
+
+    await expect(errorMessage).toBeVisible();
+    
 });
 
+test(`should fail login due to missing email input`, async ({ page }) => {
 
+    await page.goto(`/login`);
+    
+    await page.fill(`input[type="password"]`, `wrongpassword` );
+    
+    await page.evaluate(() => {
+        document.querySelector('input[type="email"]').removeAttribute('required');
+    });
+
+    await page.click(`button[type="submit"]`);
+
+
+    const errorMessage = page.locator(`.errorMessage`)
+
+    await expect(errorMessage).toBeVisible();
+
+});
+
+test(`should fail login due to missing password`, async ({ page }) => {
+
+    await page.goto(`/login`);
+
+    await page.fill(`input[type="email"]`, `s.navarroredondo@com`)
+
+    await page.evaluate(() => {
+        document.querySelector('input[type="password"]').removeAttribute('required');
+    });
+    
+    await page.click(`button[type="submit"]`);
+
+    const errorMessage = page.locator(`.errorMessage`)
+
+    await expect(errorMessage).toBeVisible();
+});
 
 

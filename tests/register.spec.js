c8ff82d3406c0992fe226f837e6d0476ac613bb5
Sergio Navarro <s.navarroredondo@gmail.com>
2025-01-07 17:36:00 +0100

    ADD playwright tests for register function

--- tests/register.spec.js
+++ tests/register.spec.js
@@ -16,7 +16,7 @@ test(`should register successfully`, async ({ page }) => {
 
     await page.goto(`/register`);
     
-    const randomEmail = `s.navarroredondo+${Math.random()}@gmail.com`;
+    const randomEmail = `${Math.random()}@gmail.com`;
     
     await page.fill(`input[type="email"]`, randomEmail,);
 
@@ -24,20 +24,102 @@ test(`should register successfully`, async ({ page }) => {
 
     await page.click(`button[type="submit"]`);
 
-    // TODO - ADD EXPECTATION -- 
-    //? Should be a API response?
+    await expect(page).toHaveURL('/login');
+});
 
+
+test(`should fail registration because user already exists`, async ({ page }) => {
+    
+    await page.goto(`/register`);
+
+    await page.fill(`input[type="email"]`, `s.navarroredondo@gmail.com` );
+
+    await page.fill(`input[type="password"]`, `1234` );
+
+    await page.click(`button[type="submit"]`);
+
+    const errorMessage = page.locator(`.errorMessage`)
+
+    await expect(errorMessage).toBeVisible();
 
 });
 
 
-test(`should fail registration`, async ({ page }) => {
+test(`should fail registration because email input is empty`, async ({ page }) => {
     
     await page.goto(`/register`);
 
-    await page.fill(`input[type="email"]`, `s.navarroredondo@com`)
-    await page.fill(`input[type="password"]`, `wrongpassword` );
+    const randomPassword = Math.random() * 1000000000;
 
-     // TODO - ADD EXPECTATION -- 
-    //? Should be a API response?
+    await page.evaluate(() => {
+        document.querySelector('input[type="email"]').removeAttribute('required');
+    });
+
+    await page.fill(`input[type="password"]`, randomPassword.toString() );
+
+    await page.click(`button[type="submit"]`);
+
+    const errorMessage = page.locator(`.errorMessage`)
+
+    await expect(errorMessage).toBeVisible();
+
+});
+
+test(`should fail registration because password is empty`, async ({ page }) => {
+    
+    await page.goto(`/register`);
+
+    const randomEmail = `${Math.random()}@gmail.com`;
+
+    await page.fill(`input[type="email"]`, randomEmail );
+
+    await page.evaluate(() => {
+        document.querySelector('input[type="password"]').removeAttribute('required');
+    });
+
+    await page.click(`button[type="submit"]`);
+
+
+    const errorMessage = page.locator(`.errorMessage`)
+
+    await expect(errorMessage).toBeVisible();
+
+});
+
+
+test(`should fail registration because email address rules is not valid`, async ({ page }) => {
+    
+    
+
+    const invalidEmails = [
+        "username@com",
+
+        //* invalid email addresses Below are already being checked by the browser with <input="email">
+        // "plainaddress",
+        // "@missingusername.com",
+        // "username@.com",
+        // "username@domain..com",
+        // "username@domain,com", 
+        // "username@domain@domain.com",
+        // "username@domain.com (Joe Smith)",
+        // "username@domain..com",
+        // "username@domain.com."
+    ];
+
+
+    for (const email of invalidEmails) {
+
+        await page.goto(`/register`);
+        
+        await page.fill(`input[type="email"]`, email );
+
+        await page.fill(`input[type="password"]`, `1234` );
+
+        await page.click(`button[type="submit"]`);
+
+        const errorMessage = page.locator(`.errorMessage`)
+
+        await expect(errorMessage).toBeVisible();
+    }
+
 });

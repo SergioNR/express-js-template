a5d1d7002482022c7eecc88747a262206e304c64
Sergio Navarro <s.navarroredondo@gmail.com>
2025-01-07 17:35:49 +0100

    ADD Front End error messages on register

--- server/controllers/userController.mjs
+++ server/controllers/userController.mjs
@@ -7,15 +7,15 @@ import { validateEmail } from "../utils/validators/emailValidation.mjs";
 export const registerUser = async (req, res) => {
 
     if (!req.body.userEmail) {
-        return res.status(400).send("Email is required");
+        return res.render(`register.ejs`, { message: `Please input an email address` });
     };
 
     if (!validateEmail(req.body.userEmail)) {
-        return res.status(400).send("Please enter a valid email address");
+        return res.render(`register.ejs`, { message: `Please enter a valid email address` });
     };
 
     if (!req.body.userPassword) {
-        return res.status(400).send("Password is required");
+        return res.render(`register.ejs`, { message: `Please input a password` });
     };
     
     const newUser = new User(req);
@@ -24,14 +24,14 @@ export const registerUser = async (req, res) => {
     
     if (await getUserByEmail(newUser.userDetails.email)) {
 
-        return res.status(400).send("User already exists");
+        return res.render(`register.ejs`, { message: `A user with that email address already exists` });
 
     } else {
         const createdUser = await createUserInDB(newUser);
     
         await posthogUserSignedUp(createdUser);
     
-        res.redirect("/user/");
+        res.redirect("/login");
     }
     
 };

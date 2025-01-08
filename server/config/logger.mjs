6ea81753c6d018b502eaf0671a3b714aea62a4b8
Sergio Navarro <s.navarroredondo@gmail.com>
2025-01-08 22:34:51 +0100

    REFACTOR logging code to better handle env variables &  load each target

--- server/config/logger.mjs
+++ server/config/logger.mjs
@@ -6,61 +6,37 @@ const redactOptions = {
 };
 
 
-export const logger = process.env.NODE_ENV === 'production' ? pino({
-    transport: {
-        target: 'pino-pretty', // Correctly specify the target as a string
-        options: {
-            colorize: true, // Optional: colorize the output for better readability
-            ignore: 'pid,hostname', // Optional: ignore the pid and hostname fields
-        },
-    },
-    transport: {
-        target: `@logtail/pino`,
-        options: {
-            sourceToken: process.env.PINOJS_PROD,
-        }
-    }
-}) : process.env.HOSTNAME === `localhost` ? pino({
+export const logger = pino({
     redact: redactOptions,
     transport: {
-        targets: [ 
-                    {
-                    target: 'pino-mongodb',
-                    options: {
-                        uri: process.env.MONGODB_CONNECTIONSTRING,
-                        database: 'logs',
-                        collection: 'log-collection',
-                        mongoOptions: {
-                            auth: {
-                                username: process.env.MONGODB_USERNAME,
-                                password: process.env.MONGODB_PASSWORD
-                                }
-                            }
+        targets: [
+            {
+                target: 'pino-mongodb',
+                options: {
+                    uri: process.env.MONGODB_CONNECTIONSTRING,
+                    database: 'logs',
+                    collection: 'log-collection',
+                    mongoOptions: {
+                        auth: {
+                            username: process.env.MONGODB_USERNAME,
+                            password: process.env.MONGODB_PASSWORD
                         }
-                    },
-                    {
-                    target: 'pino-pretty',
-                    options: {
-                        colorize: true,
-                        ignore: 'pid,hostname',
-                        },
                     }
-
-                ]
+                }
             },
-        
-}) : pino({ //* 
-    transport: {
-        target: 'pino-pretty', // Correctly specify the target as a string
-        options: {
-            colorize: true, // Optional: colorize the output for better readability
-            ignore: 'pid,hostname', // Optional: ignore the pid and hostname fields
-        },
+            ...(process.env.NODE_ENV !== 'localhost' ? [{
+                target: '@logtail/pino',
+                options: {
+                    sourceToken: process.env.PINOJS_SOURCE_TOKEN,
+                }
+            }] : []),
+            ...(process.env.NODE_ENV === 'localhost' ? [{
+                target: 'pino-pretty',
+                options: {
+                    colorize: true,
+                    ignore: 'pid,hostname',
+                }
+            }] : []),
+        ]
     },
-    transport: {
-        target: `@logtail/pino`,
-        options: {
-            sourceToken: process.env.PINOJS_DEV,
-        }
-    }
 });

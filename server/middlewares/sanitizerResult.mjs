374736ca6a62cd7a15993afd2773e2dc6a324638
Sergio Navarro <s.navarroredondo@gmail.com>
2025-01-19 00:42:44 +0100

    ADD redaction to sensitive fields in sanitizerResults middleware
    This has been done to avoid showing sensitive info on the error array

--- server/middlewares/sanitizerResult.mjs
+++ server/middlewares/sanitizerResult.mjs
@@ -1,10 +1,28 @@
 import { validationResult } from 'express-validator';
 
-export const sanitizerResult = (req, res, next) => {
-    const result = validationResult(req);
-    if (!result.isEmpty()) {
+const sanitizeValidationError = (error) => {
+  // List of fields to hide
+  const sensitiveFields = ['password', 'confirmPassword', 'token', 'email'];
 
-        return res.render('register.ejs', { message: result.errors });
-    }
-    next();
-}
+  // Create a copy of the error object
+  const sanitizedError = { ...error };
+
+  // Hide value if it's a sensitive field
+  if (sensitiveFields.includes(error.path)) {
+    sanitizedError.value = '[REDACTED]';
+  }
+
+  return sanitizedError;
+};
+
+export const sanitizerResult = (req, res, next) => {
+  const result = validationResult(req);
+  if (!result.isEmpty()) {
+    const sanitizedErrors = result.errors.map(sanitizeValidationError);
+    return res.status(400).json({
+      success: false,
+      errors: sanitizedErrors,
+    });
+  }
+  next();
+};

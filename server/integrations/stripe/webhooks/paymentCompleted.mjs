2aa654dcfd815af2d9d62d57bd5140effd831eaf
Sergio Navarro <s.navarroredondo@gmail.com>
2025-01-20 02:14:52 +0100

    ADD eslint to stripe webhook

--- server/integrations/stripe/webhooks/paymentCompleted.mjs
+++ server/integrations/stripe/webhooks/paymentCompleted.mjs
@@ -1,25 +1,19 @@
 // https://docs.stripe.com/webhooks
 
-
 export const paymentCompletedWebhook = async (req, res) => { // * Should probably add a more specific name but cant think of any since all the stripe events are handled in this function
-    const event = req.body;
-     // Handle each type of event
-    switch (event.type) {
-        case 'checkout.session.completed':
-        
-            const clientReferenceId = event.data.object.client_reference_id;
-            const variable1 = event.data.object.variable1;
+  const event = req.body;
+  // Handle each type of event
+  switch (event.type) {
+    case 'checkout.session.completed':
 
+      const clientReferenceId = event.data.object.client_reference_id;
+      const { variable1 } = event.data.object;
 
-        break;
+      break;
       // ... handle other event types
-        default:
-    console.log(`Unhandled event type ${event.type}`);
-    }
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
 
-    res.status(200).send();
+  res.status(200).send();
 };
-
-
-
-

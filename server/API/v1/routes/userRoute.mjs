dd46ed6fdae61ece724869396fab524bdeb72c1f
Sergio Navarro <s.navarroredondo@gmail.com>
2025-01-29 17:38:48 +0100

    REFACTOR auth logic into authRouter & user logic into userRoute

---
+++ server/API/v1/routes/userRoute.mjs
@@ -0,0 +1,87 @@
+import { Router } from 'express';
+import { checkSchema } from 'express-validator';
+import {
+  getUsersByFilter,
+  getOneUserById,
+} from '../../../services/userService.mjs';
+import { sanitizerResult } from '../../../middlewares/sanitizerResult.mjs';
+import { userIdInputValidator } from '../../../utils/validators/userIdInputValidator.mjs';
+
+export const userApi = Router();
+
+userApi.get('/', async (req, res) => {
+  const users = await getUsersByFilter(req);
+
+  if (!users || users.success === false) {
+    return res.status(404).json({
+      success: false,
+      message: 'Users not found',
+    });
+  }
+
+  return res.status(200).json({
+    success: true,
+    users: users,
+  });
+});
+
+userApi.get('/:userId', checkSchema(userIdInputValidator), sanitizerResult, async (req, res) => {
+  if (req.sanitizedErrors) {
+    return res.status(400).json({
+      success: false,
+      message: 'Invalid userId',
+      errors: req.sanitizedErrors,
+    });
+  }
+
+  const user = await getOneUserById(req);
+
+  if (!user || user.success === false) {
+    return res.status(404).json({
+      success: false,
+      message: 'User not found',
+    });
+  }
+  return res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
+// userApi.patch('/:userId', async (req, res) => {
+//   const user = await getUserById(req.params.userId);
+//   if (!user) {
+//     return res.status(404).json({ success: false, message: 'User not found' });
+//   }
+//   return res.status(200).json({ success: true, data: user });
+// });
+
+userApi.delete('/:userId', checkSchema(userIdInputValidator), sanitizerResult, async (req, res) => {
+  if (req.sanitizedErrors) {
+    return res.status(400).json({
+      success: false,
+      message: 'Invalid userId',
+      errors: req.sanitizedErrors,
+    });
+  }
+
+  return res.status(200).json({
+    message: 'this route is WIP',
+  });
+
+  // const user = await getUserById(req.params.userId);
+  // if (!user) {
+  //   return res.status(404).json({ success: false, message: 'User not found' });
+  // }
+  // return res.status(200).json({ success: true, data: user });
+});
+
+userApi.use('/*fallback', (req, res) => {
+  res.send('requested route does not exist in /user/');
+});
+
+// userRouter.get("/:userId/", getUser);
+/*
+* Gets the profile information of the user - we are searching
+* by userId because this is an internal api where we have the userId
+*/
--- server/routers/authRouter.mjs
+++ server/routers/authRouter.mjs
@@ -1,3 +1,84 @@
 import { Router } from 'express';
+import { checkSchema } from 'express-validator';
+import { registerUser } from '../controllers/userController.mjs';
+import { authenticationChecker } from '../middlewares/isAuthenticated.mjs';
+import { sanitizerResult } from '../middlewares/sanitizerResult.mjs';
+import { createUserValidationSchema } from '../utils/validators/createUserSchema.mjs';
+import { updatePasswordSchema } from '../utils/validators/updatePasswordSchema.mjs';
 
 export const authRouter = Router();
+
+authRouter.post('/register/', checkSchema(createUserValidationSchema), sanitizerResult, async (req, res) => {
+  if (req.sanitizedErrors) {
+    return res.status(400).json({
+      success: false,
+      errors: req.sanitizedErrors,
+    });
+  }
+
+  // const userCreation = await createUser(req);
+
+  // return res.status(200).json(userCreation);
+
+  return res.status(200).json({
+    success: true,
+    message: 'This route is a WIP',
+  });
+});
+
+authRouter.patch('/updateUserPassword', checkSchema(updatePasswordSchema), sanitizerResult, async (req, res) => {
+  if (req.sanitizedErrors) {
+    return res.status(400).json({
+      success: false,
+      message: req.sanitizedErrors,
+    });
+  }
+
+  const passwordUpdate = await updateUserPassword(req);
+
+  if (passwordUpdate.success === false && passwordUpdate.ERR_CODE === 'INCORRECT_PASSWORD') {
+    return res.status(401).json({
+      success: false,
+      message: passwordUpdate.message,
+    });
+  }
+
+  if (passwordUpdate.success === false) {
+    return res.status(400).json({
+      success: false,
+      message: passwordUpdate.message,
+    });
+  }
+
+  return res.status(200).json({
+    success: true,
+    message: 'Password updated',
+  });
+});
+
+authRouter.post('/forgot-password', async (req, res) => {
+  //
+
+  res.status(200).json({
+    success: true,
+    message: 'This route is a WIP',
+  });
+});
+
+authRouter.post('/reset-password', async (req, res) => {
+  //
+
+  res.status(200).json({
+    success: true,
+    message: 'This route is a WIP',
+  });
+});
+
+authRouter.get('/', authenticationChecker, (req, res) => {
+  res.set('Cache-Control', 'no-store');
+  res.render('userDashboard.ejs', { user: req.user, title: 'User Dashboard', description: 'User Dashboard' });
+});
+
+authRouter.get('/*fallback', (req, res) => {
+  res.status(404).send('requested API Route does not exist in the userRouter');
+});

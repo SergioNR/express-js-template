05925195fbaab7a3492bd46e49450b63accd701b
Sergio Navarro <s.navarroredondo@gmail.com>
2025-02-03 17:19:16 +0100

    ADD UUID property to User class

--- server/utils/classes/User.mjs
+++ server/utils/classes/User.mjs
@@ -1,5 +1,8 @@
+import crypto from 'crypto';
+
 export class User {
   constructor(reqBody) {
+    this.id = crypto.randomUUID();
     this.userDetails = {
       email: reqBody.username,
       password: reqBody.password,

445dc7137d3e386480d2d1d5367af1b9945b7649
Sergio Navarro <s.navarroredondo@gmail.com>
2025-02-25 18:40:51 +0100

    DELETE migration files since they are of no use in a template

--- prisma/migrations/20250204154649_update_user_model_2/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `name` on the `User` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "User" DROP COLUMN "name",
-ALTER COLUMN "last_login_at" DROP NOT NULL;

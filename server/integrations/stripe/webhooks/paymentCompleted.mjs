e8d84cff65a1e72eacdaf75256f216c71b8a544b
Sergio Navarro <s.navarroredondo@gmail.com>
2025-02-26 21:23:52 +0100

    COMMENT OUT unused code in stripe webhooks

--- server/integrations/stripe/webhooks/paymentCompleted.mjs
+++ server/integrations/stripe/webhooks/paymentCompleted.mjs
@@ -6,12 +6,12 @@ export const paymentCompletedWebhook = async (req, res) => { // * Should probabl
 // * more specific name but cant think of any since all the stripe
 // * events are handled in this function
   const event = req.body;
+
+  // const { variable1 } = event.data.object;
+  // const clientReferenceId = event.data.object.client_reference_id;
   // Handle each type of event
   switch (event.type) {
     case 'checkout.session.completed':
-
-      const clientReferenceId = event.data.object.client_reference_id;
-      const { variable1 } = event.data.object;
 
       break;
       // ... handle other event types

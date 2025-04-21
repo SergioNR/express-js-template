e7d7508728c7d03d24c7c8711b63d241ca7d729f
Sergio Navarro <s.navarroredondo@gmail.com>
2025-04-21 12:44:59 +0200

    ADD schema sanitizer to login local

--- server/API/v1/routes/authRouter.mjs
+++ server/API/v1/routes/authRouter.mjs
@@ -3,13 +3,15 @@ import { checkSchema } from 'express-validator';
 import { createUser, updateRecoveredUserPassword, updateUserPassword } from '../../../controllers/userController.mjs';
 import { sanitizerResult } from '../../../middlewares/sanitizerResult.mjs';
 import { createUserValidationSchema } from '../../../utils/validators/createUserSchema.mjs';
+import { userLoginValidationSchema } from '../../../utils/validators/userLoginValidationSchema.mjs';
+
 import { updatePasswordSchema } from '../../../utils/validators/updatePasswordSchema.mjs';
 import { logError } from '../../../config/loggerFunctions.mjs';
 import { checkSession, forgotPasswordRequest, loginLocal } from '../../../controllers/authController.mjs';
 
 export const authRouter = Router();
 
-authRouter.post('/login/local', loginLocal);
+authRouter.post('/login/local', checkSchema(userLoginValidationSchema), sanitizerResult, loginLocal);
 
 authRouter.post('/logout', (req, res, next) => {
   req.logout((err) => {
